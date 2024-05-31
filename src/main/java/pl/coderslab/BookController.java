54f66aaa9d4fccbbf598df0d2bae20fe0ba3b68a
mateuszmarc <mmarckiewicz@gmail.com>
2024-05-31 21:52:19 +0000

    Create addBook method in BookController Add requests in api-request to test addBook method

--- src/main/java/pl/coderslab/BookController.java
+++ src/main/java/pl/coderslab/BookController.java
@@ -4,10 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -39,6 +36,31 @@ public class BookController {
     public ResponseEntity<Book> findBookById(@PathVariable long id) {
         Optional<Book> optionalBook = mockBookService.get(id);
         return optionalBook.map(ResponseEntity::ok).orElseGet(() -> getBookNotFoundErrorMessage(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Book> addBook(@RequestBody Book book) {
+        log.info("{}", book);
+
+        if (doesBookToAddContainsNullValues(book)) {
+            mockBookService.add(book);
+            return ResponseEntity.ok(book);
+        }
+
+        return getBadRequestForBookAdditionError();
+    }
+
+    private static ResponseEntity<Book> getBadRequestForBookAdditionError() {
+        log.info("All fields of book instance must be populated to add book to the list");
+        return ResponseEntity.badRequest().build();
+    }
+
+    private boolean doesBookToAddContainsNullValues(Book book) {
+        return book.getIsbn() != null
+                && book.getAuthor() != null
+                && book.getType() != null
+                && book.getPublisher() != null
+                && book.getTitle() != null;
     }
 
     private static ResponseEntity<Book> getBookNotFoundErrorMessage(long id) {
