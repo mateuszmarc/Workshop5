f9233573d7bbcd422a45d93acf1a26a715c0fb9e
mateuszmarc <mmarckiewicz@gmail.com>
2024-05-31 17:59:02 +0000

    Create addBook method in BookController

--- src/main/java/pl/coderslab/BookController.java
+++ src/main/java/pl/coderslab/BookController.java
@@ -4,10 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -44,5 +41,30 @@ public class BookController {
     private static ResponseEntity<Book> getBookNotFoundErrorMessage(long id) {
         log.info("Could not find book with id: %s".formatted(id));
         return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Book> addBook(@RequestBody Book book) {
+        log.info("{}", book);
+
+        if (checkRequestBody(book)) {
+            mockBookService.add(book);
+            return ResponseEntity.ok(book);
+        }
+
+        return getBadRequestForBookAdditionError();
+    }
+
+    private static ResponseEntity<Book> getBadRequestForBookAdditionError() {
+        log.info("All fields of book instance must be populated to add book to the list");
+        return ResponseEntity.badRequest().build();
+    }
+
+    private boolean checkRequestBody(Book book) {
+        return book.getIsbn() != null
+                && book.getAuthor() != null
+                && book.getType() != null
+                && book.getPublisher() != null
+                && book.getTitle() != null;
     }
 }
