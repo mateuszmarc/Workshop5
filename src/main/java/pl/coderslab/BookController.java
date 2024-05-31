cf7d213bdfc5f84cafd33ca16b838624d3d03ddb
mateuszmarc <mmarckiewicz@gmail.com>
2024-05-31 17:26:20 +0000

    Create findBookById method in BookController Change logging mechanism to use Slf4j

--- src/main/java/pl/coderslab/BookController.java
+++ src/main/java/pl/coderslab/BookController.java
@@ -1,19 +1,21 @@
 package pl.coderslab;
 
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/books")
 public class BookController {
-    private static final Logger logger = LoggerFactory.getLogger(BookController.class);
     private final MockBookService mockBookService;
 
     @RequestMapping("/helloBook")
@@ -21,12 +23,24 @@ public class BookController {
 
         Book book =  new Book(1L, "9788324631766", "Thinking in Java",
                 "Bruce Eckel", "Helion", "programming");
-        logger.info("{}", book);
+        log.info("{}", book);
         return book;
     }
 
     @GetMapping
     public List<Book> readAllBooks() {
         return mockBookService.getBooks();
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> findBookById(@PathVariable long id) {
+        Optional<Book> optionalBook = mockBookService.get(id);
+        return optionalBook.map(ResponseEntity::ok).orElseGet(() -> getBookNotFoundErrorMessage(id));
+    }
+
+    private static ResponseEntity<Book> getBookNotFoundErrorMessage(long id) {
+        log.info("Could not find book with id: %s".formatted(id));
+        return ResponseEntity.notFound().build();
     }
 }
