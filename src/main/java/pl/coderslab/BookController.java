fcec518482822a1bc8dc72c5ae289dd75e4a6fce
mateuszmarc <mmarckiewicz@gmail.com>
2024-05-31 17:30:28 +0000

    fix import of HttpStatus

--- src/main/java/pl/coderslab/BookController.java
+++ src/main/java/pl/coderslab/BookController.java
@@ -1,8 +1,8 @@
 package pl.coderslab;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
